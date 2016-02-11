a5bf4178d281a3a46b20867044e4359d7374b855
Martin <yuwang86ss@gmail.com>
2016-02-11 12:55:45 -0800

    add

--- src/com/martin/lc/PowerOfThree.java
+++ src/com/martin/lc/PowerOfThree.java
@@ -3,11 +3,16 @@ package com.martin.lc;
 public class PowerOfThree {
 	public static boolean isPowerOfThree(int n) {
         if(n<1) return false;
-        return Math.pow(3, Math.log((double)n)/Math.log((double)3)) == n;
+        double eps = 1E-10;
+        double wah = Math.log((double)n)/Math.log((double)3);
+        int In = (int) Math.round(wah);
+        return Math.abs(wah - (double)In) < eps;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean res = isPowerOfThree(8);
+		double wahaa = 1.999999999;
+		int wahi= (int) Math.round(wahaa);
+		boolean res = isPowerOfThree(243);
 		System.out.println();
 	}
 
