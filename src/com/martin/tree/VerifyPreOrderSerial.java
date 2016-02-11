4dd1acd2e4c8c9c1af45f11e483f2454320cd611
Martin Wang <mwang@quantcast.com>
2016-02-11 11:38:09 -0800

    Check in the latest code

--- src/com/martin/tree/VerifyPreOrderSerial.java
+++ src/com/martin/tree/VerifyPreOrderSerial.java
@@ -1,11 +1,45 @@
 package com.martin.tree;
 
+import java.util.Stack;
+
 public class VerifyPreOrderSerial {
+	public static class Node {
+		public String val;
+		public int childCnt;
+		public Node(String name) {
+			this.val = name;
+			this.childCnt = 0;
+		}
+	}
+	
 	public static boolean isValidSerialization(String preorder) {
         if(preorder==null || preorder.length() == 0) return false;
         String[] data = preorder.split(",");
- 		return checkIfPre(data,0,data.length-1);
+ 		return checkUsingStack(data);
     }
+	
+	public static boolean checkUsingStack(String[] data) {
+		Stack<Node> stk = new Stack<Node>();
+		
+		for(int i=0;i<data.length;i++) {
+			if(!data[i].equals("#")) {
+				stk.push(new Node(data[i]));
+			}else{
+				if(stk.size()==0) return false;
+				stk.peek().childCnt++;
+				while(!stk.empty()) {
+					if(stk.peek().childCnt == 2) {
+						stk.pop();
+						if(!stk.isEmpty()) {
+							stk.peek().childCnt ++;
+						}
+					}else break;
+				}
+			}
+		}
+		
+		return stk.isEmpty();
+	}
 	
 	public static boolean checkIfPre(String[] data, int start, int end) {
 		if(start > end ) return false;
@@ -26,7 +60,7 @@ public class VerifyPreOrderSerial {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String waha = "9,#,#,1";
+		String waha = "9,9,9,9,9,9,9,9,#,9,#,#,9,#,9,#,#,#,9,9,#,9,#,#,9,#,9,#,#,9,9,9,#,9,#,#,9,#,#,9,#,#,9,9,9,9,9,#,#,#,9,9,#,9,9,9,9,#,#,#,#,9,#,9,9,#,#,#,9,#,#,9,9,9,#,#,9,9,#,#,9,9,#,#,#,9,#,9,9,9,9,#,#,#,#,9,#,#,9,9,9,#,9,#,#,9,9,9,#,#,9,#,#,9,9,#,9,#,9,9,#,#,#,9,#,#,9,9,#,9,#,#,9,#,#,9,9,9,#,#,#,9,9,9,9,#,9,#,#,9,#,#,9,9,#,9,#,#,9,9,#,#,9,#,#,#,9,9,9,#,#,9,#,#,9,9,9,#,#,9,9,#,#,#,9,#,9,#,9,#,#";
 		boolean res = isValidSerialization(waha);
 		System.out.println();
 	}
